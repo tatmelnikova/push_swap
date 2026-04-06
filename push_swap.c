ed0a515db008b8e1edc70978192b12f8840c7dd2
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-04-06 19:16:49 +0200

    fix main

--- push_swap.c
+++ push_swap.c
@@ -40,28 +40,30 @@ int	main(int argc, char *argv[])
 	int	strategy;
 	int	bench;
 
-	// int args[10]; // 2 1 3 6 5 8
-	// args[0] = 2;
-	// args[1] = 1;
-	// args[2] = 3;
-	// args[3] = 6;
-	// args[4] = 5;
-	// args[5] = 8;
-	// args[6] = -1;
-	// args[7] = 10;
-	// args[8] = 100;
-	// args[9] = 2;
+	int args[10]; // 2 1 3 6 5 8
+	args[0] = 2;
+	args[1] = 1;
+	args[2] = 3;
+	args[3] = 6;
+	args[4] = 5;
+	args[5] = 8;
+	args[6] = -1;
+	args[7] = 10;
+	args[8] = 100;
+	args[9] = 2;
 	// printf("args = %d\n", argc);
 	int		*numbers;
+	stack = (t_stack_holder *)malloc(sizeof(t_stack_holder));
 	if (argc == 2)
 	{
 		char **splited_num = ft_split(argv[1], ' ');
 		numbers = parse_numbers(splited_num, count_words(argv[1], ' '));
-		stack = (t_stack_holder *)malloc(sizeof(t_stack_holder));
+		
 		init_stack_holder(stack, numbers, count_words(argv[1], ' '));
 	}
-	else if (argc > 2)
+	else
 	{
+		init_stack_holder(stack, args, 10);
 		strategy = get_strategy(argc, argv);
 		bench = get_bench(argc, argv);
 	}
