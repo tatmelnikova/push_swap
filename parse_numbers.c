0e29cbf776f502adb9ba771cfff0a6875f115366
Svetlana Koreneva <skorenev@student.codam.nl>
2026-04-06 17:27:10 +0200

    add flags in numbers parsing

--- parse_numbers.c
+++ parse_numbers.c
@@ -1,17 +1,33 @@
 #include "push_swap.h"
 
+static int	is_keyword(char *word)
+{
+	return (!strcmp(word, "--simple") || !strcmp(word, "--medium") || !strcmp(word, "--complex") 
+			|| !strcmp(word, "--adaptive") || !strcmp(word, "--bench"));
+}
+
 int	*parse_numbers(char **s_numbers, int size)
 {
 	int	*numbers;
 	int	i;
+	int	num_count;
 
 	i = 0;
-	numbers = malloc(sizeof(int *) * size);
-	if (!numbers)
-		return (NULL);
+	num_count = size;
 	while (i < size)
 	{
-		numbers[i] = ft_atoi(s_numbers[i]);
+		if (is_keyword(s_numbers[i]))
+			num_count--;
+		i++;
+	}
+	numbers = malloc(sizeof(int *) * num_count);
+	if (!numbers)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{	
+		if (!is_keyword(s_numbers[i]))
+			numbers[i] = ft_atoi(s_numbers[i]);
 		i++;
 	}
 	return (numbers);
