69d3e62c928593f7f4b9873bebf0fa6f82d12443
Svetlana Koreneva <skorenev@student.codam.nl>
2026-04-06 14:47:15 +0200

    new bubble version

--- bubble_sort.c
+++ bubble_sort.c
@@ -4,6 +4,7 @@ static t_stack_holder	*return_in_stack_a(t_stack_holder *holder)
 {
 	while (holder->b_count > 0)
 		pa(holder);
+
 	return (holder);
 }
 
@@ -30,29 +31,30 @@ static int	sort_check(t_stack_holder *holder)
 
 t_stack_holder	*bubble_sort(t_stack_holder *holder)
 {
+	int	a_count;
+	int	i;
+	int j;
 	t_stack	*first;
 	t_stack	*second;
-	int		i;
-	
-	while (holder->a_count > 1)
+
+	a_count = holder->a_count;
+	j = 0;
+	while (j < a_count)
 	{
 		i = 0;
-		
-		while (i <= holder->a_count)
+		while (i < a_count - j - 1)
 		{
 			first = holder->a;
 			second = first->next;
-			if (first->content < second->content)
-
+			if (first->content > second->content)
 				sa(holder);
-			ra(holder);
+			pb(holder);
 			i++;
 		}
+		return_in_stack_a(holder);
 		if (sort_check(holder))
 			break ;
-		pb(holder);
+		j++;
 	}
-	return_in_stack_a(holder);
-	print_stack_holder(holder);
-	return (holder);
+	return(holder);
 }
