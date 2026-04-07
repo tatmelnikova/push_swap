65b9debd22b277d81f3a6cc7a01bc4bfb2312b0e
Svetlana Koreneva <skorenev@student.codam.nl>
2026-04-07 15:33:37 +0200

    parse numbers from argv to num

--- push_swap.c
+++ push_swap.c
@@ -8,7 +8,29 @@ void test_sa(t_stack_holder	*stack)
 	// 	return (0);
 	// }
 	print_stack_holder(stack);
-	bubble_sort(stack);
+	// sa(stack);
+	// print_stack_holder(stack);
+	// // pb(stack);
+	// // pb(stack);
+	// // pb(stack);
+	// // pb(stack);
+	// // print_stack_holder(stack);
+	// // pa(stack);
+	// ra(stack);
+	// print_stack_holder(stack);
+	// rra(stack);
+	// print_stack_holder(stack);
+	// pb(stack);
+	// pb(stack);
+	// pb(stack);
+	// print_stack_holder(stack);
+	// rrb(stack);
+	// print_stack_holder(stack);
+	// rrb(stack);
+	// print_stack_holder(stack);
+	// rrr(stack);
+	// print_stack_holder(stack);
+	bubble_sort(stack); //test
 	print_stack_holder(stack);
 }
 
@@ -18,39 +40,37 @@ int	main(int argc, char *argv[])
 	int	strategy;
 	int	bench;
 
-	int args[10]; // 2 1 3 6 5 8
-	args[0] = 2;
-	args[1] = 1;
-	args[2] = 3;
-	args[3] = 6;
-	args[4] = 5;
-	args[5] = 8;
-	args[6] = -1;
-	args[7] = 10;
-	args[8] = 100;
-	args[9] = 2;
+	// int args[10]; // 2 1 3 6 5 8
+	// args[0] = 2;
+	// args[1] = 1;
+	// args[2] = 3;
+	// args[3] = 6;
+	// args[4] = 5;
+	// args[5] = 8;
+	// args[6] = -1;
+	// args[7] = 10;
+	// args[8] = 100;
+	// args[9] = 2;
 	// printf("args = %d\n", argc);
 	int		*numbers;
-	stack = (t_stack_holder *)malloc(sizeof(t_stack_holder));
-	if (argc == 2)
+	int i;
+
+	i = 0;
+	if (argc > 1)
 	{
-		char **splited_num = ft_split(argv[1], ' ');
-		numbers = parse_numbers(splited_num, count_words(argv[1], ' '));
-		
-		init_stack_holder(stack, numbers, count_words(argv[1], ' '));
-	}
-	else
-	{
-		init_stack_holder(stack, args, 10);
 		strategy = get_strategy(argc, argv);
 		bench = get_bench(argc, argv);
-		print_args(argc, argv);
-		float disorder = compute_disorder(stack->a); 
-		print_disorder(disorder);
+		numbers = parse_numbers(&argv[1], argc - 1);
+		stack = (t_stack_holder *)malloc(sizeof(t_stack_holder));
+		init_stack_holder(stack, numbers, argc - 1);
+		print_stack_holder(stack);
 	}
 	test_sa(stack);
-	if (bench)
-		print_bench(stack);
-	return (strategy == bench);
-}
+	// stack = (t_stack_holder *)malloc(sizeof(t_stack_holder));
+	// init_stack_holder(stack, numbers, count_words(argv[1], ' '));
 
+	// test_sa(stack);
+	// if (bench)
+	// 	print_bench(stack);
+	// return (strategy == bench);
+}
